fb6ac5d0017e53b92f268ff72a02294f19b2fad0
Anshuman-Programmer <anshumantalukdar02@gmail.com>
2024-10-15 00:00:38 +0530

    env example added

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import app from "./index";
 
 // Define the port to run on, default to 5000 if not specified in the environment variables
 const port = process.env.PORT || 5000;
+console.log(process.env.PORT);
 
 // Start the server
 app.listen(port, () => {
